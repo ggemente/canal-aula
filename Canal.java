d5b63433749c8f70244d63f236e962abee1c5e80
ggemente <gabrielxgemente@gmail.com>
2025-09-15 20:15:44 -0300

    Resolução exercicícios (exceção opcional)

--- Canal.java
+++ Canal.java
@@ -1,13 +1,12 @@
-/**
- * Armazena os detalhes das inscrições em um canal
- * 
- * @author (seu nome)
- * @version (um número de versão ou uma data)
- */
+import java.util.ArrayList;
+
+
+
 public class Canal
 {
-    // Defina quaisquer atributos necessários aqui...
-
+    
+    private ArrayList<Inscricao> inscricoes;
+    
 
     
     /**
@@ -15,16 +14,29 @@ public class Canal
      */
     public Canal()
     {
-        // Inicialize quaisquer atributos aqui ...
-        
+        inscricoes = new ArrayList<Inscricao>();
     }
 
     /**
      * Adiciona um novo incrito na lista de inscritos do canal.
      * @param inscricao O objeto inscricao a ser adicionado.
      */
-    public void inscrever(Inscricao inscricao)
+
+     public boolean estaInscrito(String nome){
+         for (Inscricao inscrito : inscricoes) {
+            if (inscrito.obterNome().equals(nome)) {
+                System.out.println("Erro: Já existe um inscrito com o nome " + nome);
+                return false;
+            }
+        }
+        return true;
+     }
+
+    public boolean inscrever(Inscricao inscricao)
     {
+       
+        return estaInscrito(inscricao.obterNome()) && inscricoes.add(inscricao);
+
     }
 
     /**
@@ -32,6 +44,21 @@ public class Canal
      */
     public int quantidadeDeInscritos()
     {
-        return 0;
+        return inscricoes.size();
+    }
+
+    public int inscritosNomes(int mes) {
+        if (mes < 1 || mes > 12) {
+            System.out.println("Erro: mês inválido. Informe um valor entre 1 e 12.");
+            return 0;
+        }
+        int count = 0;
+        for (Inscricao inscricao : inscricoes) {
+            if (inscricao.obterMes() == mes) {
+                count++;
+            }
+        }
+        return count;
     }
 }
+
